99e674df88b064e2fda7a7d3a2169a2055a146b3
Matheus Macedo <31077763+kbzrmacedo@users.noreply.github.com>
2019-06-09 03:31:18 -0300

    Update febraban2019.py

--- febraban2019.py
+++ febraban2019.py
@@ -6,3 +6,6 @@ myapp = Flask(__name__)
 def hello():
     xml = '<Response><Say>Hello world.</Say></Response>'
     return Response(xml, mimetype='text/xml')
+
+if __name__ == '__main__':
+    app.run()
